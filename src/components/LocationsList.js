7cd415250a05c5c59e69524eaa3a1ae28fb61188
Blake <bpro0821@gmail.com>
2019-07-26 12:49:18 -0500

    added styled, created locationlist component and added to TabNav

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import LocationCard from './LocationCard'
 
 export default function LocationsList() {
+const [locations, setLocations] = useState([]);
 
-}
+
+useEffect(() => {
+   
+    axios.get(`http://rickandmortyapi.com/api/location/`)
+
+    .then(res => {
+      setLocations(res.data.results);
+      console.log(res.data.results);
+    });
+    
+    // .catch(err => {
+    //   console.log(err);
+    // });
+
+  }, [locations]);
+
+  return <section className='location-list grid-view'>
+             {locations.map(location => <LocationCard name={location.name} />)}
+         </section>
+};
+
+
+
